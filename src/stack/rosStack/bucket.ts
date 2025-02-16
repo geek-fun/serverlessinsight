01e0ad85721e5ed5761e3b2857279e259a780c3c
seven <zilisheng1996@gmail.com>
2025-02-16 22:07:26 +0800

    fix: fix the frontend deployment issue (#39)
    fix: fix the frontend deployment issue
    
    - create si defined roles for ossdeployment to fix policy name conflicts
    issue
    - update schema validation to move required property of delete_force as
    optional
    - enable support to  specify the access control of bucket
    
    Refs: #36
    
    ---------
    
    Signed-off-by: seven <zilisheng1996@gmail.com>

--- src/stack/rosStack/bucket.ts
+++ src/stack/rosStack/bucket.ts
@@ -1,9 +1,16 @@
-import { ActionContext, BucketDomain } from '../../types';
+import { ActionContext, BucketAccessEnum, BucketDomain } from '../../types';
 import * as oss from '@alicloud/ros-cdk-oss';
 import * as ros from '@alicloud/ros-cdk-core';
 import { getAssets, replaceReference } from '../../common';
 import * as ossDeployment from '@alicloud/ros-cdk-ossdeployment';
 import path from 'node:path';
+import { RosRole } from '@alicloud/ros-cdk-ram';
+
+const aclMap = new Map([
+  [BucketAccessEnum.PRIVATE, 'private'],
+  [BucketAccessEnum.PUBLIC_READ, 'public-read'],
+  [BucketAccessEnum.PUBLIC_READ_WRITE, 'public-read-write'],
+]);
 
 export const resolveBuckets = (
   scope: ros.Construct,
@@ -13,9 +20,42 @@ export const resolveBuckets = (
   if (!buckets) {
     return undefined;
   }
+  const bucketSources = buckets.some((bucket) => bucket?.website?.code);
+  let siAutoOssDeploymentBucketRole: RosRole | undefined;
+  if (bucketSources) {
+    siAutoOssDeploymentBucketRole = new RosRole(
+      scope,
+      'si_auto_od_bucket_role',
+      {
+        roleName: ros.Fn.sub('si-auto-od-bucket-role-${ALIYUN::StackId}'),
+        description:
+          'roles created by ServerlessInsight for oss deployment to put files to oss bucket during deployment',
+        deletionForce: false,
+        ignoreExisting: false,
+        assumeRolePolicyDocument: {
+          version: '1',
+          statement: [
+            {
+              action: 'sts:AssumeRole',
+              effect: 'Allow',
+              principal: { service: ['fc.aliyuncs.com'] },
+            },
+          ],
+        },
+        policyAttachments: {
+          system: ['AliyunOSSFullAccess', 'AliyunLogFullAccess'],
+        },
+      },
+      true,
+    );
+  }
+
   buckets.forEach((bucket) => {
     const ossBucket = new oss.Bucket(scope, replaceReference(bucket.key, context), {
       bucketName: replaceReference(bucket.name, context),
+      accessControl: aclMap.get(
+        replaceReference(bucket.security?.acl, context) ?? ('' as BucketAccessEnum),
+      ),
       websiteConfigurationV2: bucket.website
         ? {
             indexDocument: {
@@ -34,10 +74,11 @@ export const resolveBuckets = (
       const filePath = path.resolve(process.cwd(), replaceReference(bucket.website.code, context));
       new ossDeployment.BucketDeployment(
         scope,
-        `${replaceReference(bucket.key, context)}_bucket_code_deployment`,
+        `si_auto_${bucket.key}_bucket_code_deployment`,
         {
           sources: getAssets(filePath),
           destinationBucket: ossBucket.attrName,
+          roleArn: siAutoOssDeploymentBucketRole!.attrArn,
           timeout: 3000,
           logMonitoring: false,
           retainOnCreate: false,
