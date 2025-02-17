69376f33b7db2296f90e10dd7947b112d1c7127f
seven <zilisheng1996@gmail.com>
2025-02-18 02:43:29 +0800

    feat: setup nas resources for fc
    Signed-off-by: seven <zilisheng1996@gmail.com>

--- src/stack/rosStack/function.ts
+++ src/stack/rosStack/function.ts
@@ -1,4 +1,4 @@
-import { ActionContext, FunctionDomain, ServerlessIac } from '../../types';
+import { ActionContext, FunctionDomain, NasStorageClassEnum, ServerlessIac } from '../../types';
 import {
   CODE_ZIP_SIZE_LIMIT,
   getFileSource,
@@ -11,7 +11,18 @@ import { isEmpty } from 'lodash';
 import * as ossDeployment from '@alicloud/ros-cdk-ossdeployment';
 import * as ros from '@alicloud/ros-cdk-core';
 import * as sls from '@alicloud/ros-cdk-sls';
+import * as nas from '@alicloud/ros-cdk-nas';
 import { RosFunction } from '@alicloud/ros-cdk-fc3/lib/fc3.generated';
+
+const storageClassMap = {
+  [NasStorageClassEnum.STANDARD_CAPACITY]: { fileSystemType: 'standard', storageType: 'Capacity' },
+  [NasStorageClassEnum.STANDARD_PERFORMANCE]: {
+    fileSystemType: 'standard',
+    storageType: 'Performance',
+  },
+  [NasStorageClassEnum.EXTREME_STANDARD]: { fileSystemType: 'extreme', storageType: 'standard' },
+  [NasStorageClassEnum.EXTREME_ADVANCE]: { fileSystemType: 'extreme', storageType: 'advance' },
+};
 
 export const resolveFunctions = (
   scope: ros.Construct,
@@ -102,6 +113,26 @@ export const resolveFunctions = (
         )?.objectKey,
       };
     }
+    let fcNas: Array<{ nas: nas.FileSystem; mountDir: string }> | undefined;
+    if (fnc.storage?.nas) {
+      fcNas = fnc.storage.nas.map((nasItem) => {
+        const { fileSystemType, storageType } = storageClassMap[nasItem.storage_class];
+        const nasResource = new nas.FileSystem(
+          scope,
+          `${fnc.key}_nas`,
+          {
+            fileSystemType,
+            storageType,
+            protocolType: 'NFS',
+            description: `NAS for function ${fnc.name}`,
+          },
+          true,
+        );
+
+        return { nas: nasResource, mountDir: nasItem.mount_path ?? '/' };
+      });
+    }
+
     const fcn = new fc.RosFunction(
       scope,
       fnc.key,
@@ -111,9 +142,16 @@ export const resolveFunctions = (
         runtime: replaceReference(fnc.runtime, context),
         memorySize: replaceReference(fnc.memory, context),
         timeout: replaceReference(fnc.timeout, context),
+        diskSize: fnc.storage?.disk,
         environmentVariables: replaceReference(fnc.environment, context),
         code,
         logConfig,
+        nasConfig: {
+          mountPoints: fcNas?.map(({ nas, mountDir }) => ({
+            mountDir,
+            serverAddr: nas.getAtt('serverAddr'),
+          })),
+        },
       },
       true,
     );
--- src/types/domains/function.ts
+++ src/types/domains/function.ts
@@ -9,8 +9,39 @@ export type FunctionRaw = {
   environment?: {
     [key: string]: string;
   };
+  storage?: {
+    disk?: number;
+    nas?: Array<{
+      mount_path?: string;
+      storage_class?: string;
+    }>;
+  };
 };
 
-export type FunctionDomain = FunctionRaw & {
+export type FunctionDomain = {
   key: string;
+  name: string;
+  runtime: string;
+  handler: string;
+  code: string;
+  memory: number;
+  timeout: number;
+  log?: boolean;
+  environment?: {
+    [key: string]: string;
+  };
+  storage?: {
+    disk?: number;
+    nas?: Array<{
+      mount_path?: string;
+      storage_class: NasStorageClassEnum;
+    }>;
+  };
 };
+
+export enum NasStorageClassEnum {
+  STANDARD_PERFORMANCE = 'STANDARD_PERFORMANCE',
+  STANDARD_CAPACITY = 'STANDARD_CAPACITY',
+  EXTREME_STANDARD = 'EXTREME_STANDARD',
+  EXTREME_ADVANCE = 'EXTREME_ADVANCE',
+}
